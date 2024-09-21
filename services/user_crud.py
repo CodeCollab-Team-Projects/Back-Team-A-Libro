954094a6668cdd7b90bdcafba1a41bc9cae30aca
aleksGdz <aleksgaldamez2002@gmail.com>
2024-09-21 14:01:11 -0600

    Update for crud methods

--- services/user_crud.py
+++ services/user_crud.py
@@ -1,11 +1,15 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import select
 from models.user_models import User 
 from schemas.user_schemas import UserCreate
-import bcrypt
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 def create_user(db: Session, user: UserCreate):
-    hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
-    db_user = User(username=user.username, email=user.email, hashed_password=hashed_password.decode('utf-8'))
+    hashed_password = pwd_context.hash(user.password)
+    db_user = User(username=user.username, email=user.email, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -13,3 +17,9 @@ def create_user(db: Session, user: UserCreate):
 
 def get_user_by_username(db: Session, username: str):
     return db.query(User).filter(User.username == username).first()
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
