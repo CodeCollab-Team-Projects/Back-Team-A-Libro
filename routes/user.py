8aeb0e4c50404ba46f5cec4eb7fc7528bb3107e9
aleksGdz <aleksgaldamez2002@gmail.com>
2024-10-06 17:10:03 -0600

    Add /me PUT endpoint to include additional user data

--- routes/user.py
+++ routes/user.py
@@ -1,24 +1,21 @@
 from auth import auth
-from services import user_crud 
 from datetime import timedelta
-from config.database import get_db
+from services import user_crud 
 from sqlalchemy.orm import Session
+from config.database import get_db
+from model.user_models import User
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import APIRouter, HTTPException, status, Depends
-from schemas.user_schemas import UserCreate, User, UserResponse
-
+from schemas.user_schemas import UserCreate, UserResponse, UserUpdate, UserRegister
 
 user = APIRouter()
 
-
-@user.post("/register", response_model=UserResponse)
+@user.post("/register", response_model=UserRegister)
 def register_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = user_crud.get_user_by_username(db, user.username)
     if db_user:
         raise HTTPException(status_code=400, detail="User already registered")
     return user_crud.create_user(db=db, user=user)
-
-
 
 @user.post("/login")
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
@@ -35,8 +32,13 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:
     )
     return {"msg": "Login successful", "user": user.username, "access_token": access_token}
 
-
-
 @user.get("/me", response_model=UserResponse)
 def read_users_me(current_user: User = Depends(auth.get_current_user)):
-    return current_user
+    return user_crud.user_to_response(current_user)
+
+
+@user.put("/me", response_model=UserResponse)
+def update_user_profile(update: UserUpdate, db: Session = Depends(get_db), current_user: User = Depends(auth.get_current_user)):
+    updated_user = user_crud.update_user_profile(db, current_user, update)
+    return user_crud.user_to_response(updated_user)
+
