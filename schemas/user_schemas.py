769891a6eb43c7aa43f89ac75efb8e77f2d6bab7
aleksGdz <aleksgaldamez2002@gmail.com>
2024-10-06 16:59:09 -0600

    Add new schemas for user management

--- schemas/user_schemas.py
+++ schemas/user_schemas.py
@@ -1,25 +1,38 @@
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel, EmailStr, Field
 
 # User Schemas
-
-class User(BaseModel):
-    id: str
-    username:str
-
 class UserBase(BaseModel):
     username: str = Field(..., min_length=2, max_length=50)
     email: EmailStr
 
+class UserRegister(UserBase):
+    id: str
 
 class UserCreate(UserBase):
     password: str
 
 class UserResponse(UserBase):
     id: str
+    country: Optional[str] = None
+    language: Optional[str] = None
+    favorite_categories: List[str] = []
+    favorite_authors: List[str] = []
 
     class Config:
         from_attributes = True
+
+class UserUpdate(BaseModel):
+    country: Optional[str] = None
+    language: Optional[str] = None
+    favorite_categories: Optional[List[str]] = None  
+    favorite_authors: Optional[List[str]] = None
+
+class CategoryBase(BaseModel):
+    name: str
+
+class AuthorBase(BaseModel):
+    name: str
 
 class Token(BaseModel):
     access_token: str
@@ -27,4 +40,3 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     username: Optional[str] = None
-
